4576d4d8034f64fd8c0627a9b210a30045cd88af
Egor <5642452@mail.ru>
2019-11-14 19:41:21 +0300

    init: add all files

---
+++ codejam-image-api/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "amd": true,
+        "node": true,
+        "jest": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+    }
+};
