3428e29f7b2c896dcbb963e4871d08dfdb137029
Elaray1 <5642452@mail.ru>
2019-12-03 16:39:29 +0300

    refactor: added tabs in script.test.js

--- codejam-image-api/script.test.js
+++ codejam-image-api/script.test.js
@@ -1,11 +1,11 @@
 const convertRgbToHex = require('./script');
 
 describe('convertRgbToHex', () => {
-test('transform RGB format in HEX format (white)', () => {
-  expect(convertRgbToHex(255, 255, 255)).toBe('#ffffff');
-})
+  test('transform RGB format in HEX format (white)', () => {
+    expect(convertRgbToHex(255, 255, 255)).toBe('#ffffff');
+  });
 
-test('transform RGB format in HEX format (black)', () => {
-  expect(convertRgbToHex(0, 0, 0)).toBe('#000000');
-})
+  test('transform RGB format in HEX format (black)', () => {
+    expect(convertRgbToHex(0, 0, 0)).toBe('#000000');
+  });
 });
