1c0b685c61259e03432d6246c07f0258362231bc
Egor <5642452@mail.ru>
2019-12-10 17:00:35 +0300

    feat: add search input and styles for control block

--- fancy-weather/src/js/script.js
+++ fancy-weather/src/js/script.js
@@ -2,5 +2,5 @@ import { getUserLocation, getWeather, getBgImage } from './asyncFunctions';
 import { getYearTime, getDayTime } from './timeOfTheYear';
 
 window.onload = async function () {
-  console.log(await getBgImage());
+  
 };
